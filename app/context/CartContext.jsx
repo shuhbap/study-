197d820a643e8e7d7697c8a5ebf95efceae99f0a
Shuhaib! <127591148+shuhbap@users.noreply.github.com>
2026-05-23 19:32:18 +0530

    Create CartContext.jsx

---
+++ app/context/CartContext.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cartCount, setCartCount] = useState(0);
+
+  const addToCart = (qty = 1) => {
+    setCartCount((prev) => prev + qty);
+  };
+
+  return (
+    <CartContext.Provider value={{ cartCount, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
